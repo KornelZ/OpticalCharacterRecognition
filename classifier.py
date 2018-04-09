3586f4669ab43dd0bd21f834943a967020567619
Kornel Żaba <kornelzaba@gmail.com>
2018-04-09 23:11:56 +0200

    Added SVM testing

--- classifier.py
+++ classifier.py
@@ -1,17 +1,25 @@
 import cv2 as cv
 import numpy as np
 
+
 class Classifier(object):
 
     def __init__(self, classifier):
-
         if classifier is "SVM":
             self.classifier = cv.ml.SVM_create()
+            self.classifier.setType(cv.ml.SVM_C_SVC)
+            self.classifier.setKernel(cv.ml.SVM_INTER) #75% accuracy, no params required
+            #45$ accuracy for linear kernel
 
 
     def train(self, input):
         train_arr = np.array(input[0], np.float32)
         label_arr = np.array(input[1])
 
-        self.classifier.train(train_arr, cv.ml.ROW_SAMPLE, label_arr)
-        self.classifier.save("svm_train.dat")
+        self.classifier.train(train_arr, cv.ml.ROW_SAMPLE,
+                              label_arr)
+
+    def predict(self, input):
+        predict_arr = np.array(input[0], np.float32)
+
+        return self.classifier.predict(predict_arr)[1]
--- main.py
+++ main.py
@@ -4,14 +4,38 @@ import feature_extraction as fe
 import classifier
 from util import get_labels
 
-src = cv.imread("letters.png")
-seg = sg.Segmentation(40, 40)
-segmented = seg.segmentize(src, False)
-extraction = fe.FeatureExtraction(8)
-feature_vector = extraction.extract(segmented)
-labels = get_labels(src.shape[1], src.shape[0], 40, 40)
+DIM = 40
+EXTRACTION_STEP = 4
 
-print("TRAINING")
-training_input = [feature_vector, labels]
+
+def preprocess(input, segmentation, extraction, dim):
+    segmented = segmentation.segmentize(input, False)
+    feature_vector = extraction.extract(segmented)
+    labels = get_labels(input.shape[1], input.shape[0], dim, dim)
+    return [feature_vector, labels]
+
+
+src = cv.imread("letters.png")
+train = src[:, :src.shape[1] // 2]
+test = src[:, src.shape[1] // 2:]
+
+seg = sg.Segmentation(DIM, DIM)
+ext = fe.FeatureExtraction(EXTRACTION_STEP)
+
+
+training_input = preprocess(train, seg, ext, DIM)
+test_input = preprocess(test, seg, ext, DIM)
+
 svm = classifier.Classifier("SVM")
 svm.train(training_input)
+result = svm.predict(test_input)
+
+print(result)
+
+count = 0
+for label, predicted in zip(test_input[1], result):
+    if label == int(predicted[0]):
+        count += 1
+print(count / len(test_input[1]))
+
+
